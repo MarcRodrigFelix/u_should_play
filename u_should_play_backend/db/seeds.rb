dc336717e4d8eb7ded658bc491e4ee6da90b195a
Marcos <m.rodrig.felix@gmail.com>
2021-03-08 22:30:14 -0800

    migrations, relationships set up

--- u_should_play_backend/db/seeds.rb
+++ u_should_play_backend/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+red = Game.create(title: "Red Dead Redemption II")
+last = Game.create(title: "The Last Of Us")
+furi = Game.create(title: "Furi")
+
+Comment.create(content: "This game was so awesome I practically lived in it for weeks.", commentator: "ThatRedDeadGuuuuuy", game_id: red.id)
+Comment.create(content: "Very emotional game but, I agree. This is a must play.", commentator: "TLOUNumber1Fan", game_id: last.id)
+Comment.create(content: "One of the funnest indie games ever! Non stop action.", commentator: "IamfurI", game_id: furi.id)
