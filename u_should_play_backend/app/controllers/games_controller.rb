5f827a92b332a95ed5f4512687cd858cc95c93c5
Marcos <m.rodrig.felix@gmail.com>
2021-03-09 21:33:58 -0800

    render json for games and games comments

--- u_should_play_backend/app/controllers/games_controller.rb
+++ u_should_play_backend/app/controllers/games_controller.rb
@@ -2,7 +2,12 @@ class GamesController < ApplicationController
 
   def index
     game = Game.all
-    render json: game
+    render json: game.to_json(
+      only: [ :title, :image, :review ], 
+      include: [ 
+        comments: { 
+          only: [ :content, :commentator ]
+    }])
   end
 
 end
