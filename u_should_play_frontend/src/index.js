22b7172c69cdc1e2ecdd6688fab50e062370ee75
Marcos <m.rodrig.felix@gmail.com>
2021-03-09 23:19:49 -0800

    fetch request for all games

--- u_should_play_frontend/src/index.js
+++ u_should_play_frontend/src/index.js
@@ -1,1 +1,13 @@
-console.log("hello")
+const GAMES_URL = 'http://localhost:3000/games';
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchAllGames()
+});
+
+
+function fetchAllGames(){
+  fetch( GAMES_URL )
+  .then( response => response.json() )
+  .then( games => console.log(games) )
+  .catch( error => console.log(error.message) )
+};
