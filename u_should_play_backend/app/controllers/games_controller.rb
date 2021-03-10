8e523289f03d9624f139a5a8736378c89d29d472
Marcos <m.rodrig.felix@gmail.com>
2021-03-09 21:49:03 -0800

    create action and game_params, create resource

--- u_should_play_backend/app/controllers/games_controller.rb
+++ u_should_play_backend/app/controllers/games_controller.rb
@@ -10,4 +10,22 @@ class GamesController < ApplicationController
     }])
   end
 
+  def create
+binding.pry
+    game = Game.new(game_params)
+    if game.save
+      render json: game, status: :accepted
+    else
+      render json: {errors: game.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :image, :review)
+  end
+
 end
--- u_should_play_backend/config/routes.rb
+++ u_should_play_backend/config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   #   end
   # end
 
-  resources :games, only: [ :index ]
+  resources :games, only: [ :index, :create ]
 
 end
