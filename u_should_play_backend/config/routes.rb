32a6d8a8fc640b40eda78b4bfc4546e08af95a0a
Marcos <m.rodrig.felix@gmail.com>
2021-03-18 14:56:44 -0700

    add new action route to comment routes

--- u_should_play_backend/config/routes.rb
+++ u_should_play_backend/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # end
 
   resources :games
-  resources :comments, only: [ :index, :show ]
+  resources :comments, only: [ :index, :show, :new ]
   # , only: [ :index, :create, :update ]
 
   
