fd7a4822f2be863a0e3dbf9289a14eb743e6671c
Marcos <m.rodrig.felix@gmail.com>
2021-03-19 20:17:20 -0700

    change html syntax and css where comments are added and displayed

--- u_should_play_frontend/src/Game.js
+++ u_should_play_frontend/src/Game.js
@@ -32,12 +32,12 @@ class Game{
       </div>
         
       <div class="comments-section">
-          <h3>${this.title} Comments:</h3>
-          <p data-id=${this.id} class="add"> Add Comment </p>
+          <h3>Add Comments:</h3>
           <!-- <div data-id=${this.id} style="display:none;"> -->
             ${this.addNewComment()}
           </div>
         <div data-id="${this.id}" class="game-comments">
+        <h4>${this.title} Comments</h4>
         </div>
       </div>
     </div>`
